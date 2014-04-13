776b1adbd4239be0fd91162ec1dcf73aa736e11a
Hugo Rodger-Brown <hugo@rodger-brown.com>
2014-04-13 17:19:47 +0100

    Add DEBUG static file serving.

--- lido/urls.py
+++ lido/urls.py
@@ -1,5 +1,7 @@
-from django.conf.urls import patterns, include, url
+# main project URL definitions.
 
+from django.conf import settings
+from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.contrib.auth.views import logout
 
@@ -11,3 +13,8 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^logout/$', logout, kwargs={'next_page': '/'}, name='logout')
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('django.contrib.staticfiles.views',
+        url(r'^static/(?P<path>.*)$', 'serve'),
+    )
